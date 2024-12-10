1520f159736dbea93787be4b63dfb92f22a4a205
Ayobami-ANDROID <ajetunmobiayobami19@gmail.com>
2024-12-10 21:42:30 +0100

    prisma migrate

--- routes/todoRoute.ts
+++ routes/todoRoute.ts
@@ -6,7 +6,7 @@ const router = Router();
 router.post('/', TodoController.createTodo);
 router.get('/', TodoController.getAllTodos);
 router.get('/:id', TodoController.getTodoById);
-router.patch('/:id', TodoController.updateTodo);
+router.put('/:id', TodoController.updateTodo);
 router.delete('/:id', TodoController.deleteTodo);
 
 export default router;
