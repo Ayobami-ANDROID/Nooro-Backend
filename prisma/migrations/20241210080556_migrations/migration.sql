9edbde28603d971ebf775c4262d8c96dcf4fc549
Ayobami-ANDROID <ajetunmobiayobami19@gmail.com>
2024-12-10 09:07:11 +0100

    prisma migrate

--- prisma/migrations/20241210080556_migrations/migration.sql
+++ prisma/migrations/20241210080556_migrations/migration.sql
@@ -2,6 +2,7 @@
 CREATE TABLE `Todo` (
     `id` INTEGER NOT NULL AUTO_INCREMENT,
     `title` VARCHAR(191) NOT NULL,
+    `color` VARCHAR(191) NOT NULL,
     `completed` BOOLEAN NOT NULL DEFAULT false,
     `createdAt` DATETIME(3) NOT NULL DEFAULT CURRENT_TIMESTAMP(3),
     `updatedAt` DATETIME(3) NULL,
